41dc8ac795aefb10a12893ed0b9b138f3f0fcf3b
GladsonOfficial <gladson.thomas.official@gmail.com>
2024-06-02 18:02:27 +0530

    added readme to setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = '0.0.4'
+VERSION = '0.0.5'
 DESCRIPTION = 'Collection of automotive calculations'
 LONG_DESCRIPTION = 'This repository provides a collection of mathematical formulas commonly used in the automotive industry, with a particular focus on Brushless DC (BLDC) motors.'
+
+with open('README.md') as f:
+    long_description = f.read()
 
 # Setting up
 setup(
@@ -14,7 +17,7 @@ setup(
     author_email="<gladson.thomas.official@gmail.com>",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
-    long_description=LONG_DESCRIPTION,
+    long_description=long_description,
     packages=find_packages(),
     install_requires=[],
     keywords=['python', 'calculations', 'automotive', 'EV', 'BLDC'],
